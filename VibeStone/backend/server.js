ed18a5b99659ab06821d031a578f626a03a224c6
giangkh1908 <giangkh1908@gmail.com>
2025-09-27 16:10:18 +0700

    update cors

--- VibeStone/backend/server.js
+++ VibeStone/backend/server.js
@@ -17,17 +17,23 @@ const port = process.env.PORT || 5000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
+  // cors({
+  //   origin: [
+  //     "http://localhost:5173",
+  //     "http://localhost:5175",
+  //     "http://localhost:5000",
+  //     "https://vibe-stone-admin.vercel.app",
+  //     "https://www.vibestoneoficial.store",
+  //     "https://www.facebook.com",
+  //     "https://facebook.com",
+  //     "https://static.xx.fbcdn.net",
+  //   ],
+  //   credentials: true,
+  //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  //   allowedHeaders: ["Content-Type", "Authorization", "token"],
+  // })
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5175",
-      "http://localhost:5000",
-      "https://vibe-stone-admin.vercel.app",
-      "https://www.vibestoneoficial.store",
-      "https://www.facebook.com",
-      "https://facebook.com",
-      "https://static.xx.fbcdn.net",
-    ],
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "token"],
